4e4074578464280222cd5f3d02cf88607024dfc3
s1lv3r <phvietan@gmail.com>
2019-09-17 00:43:03 +0700

    init project

---
+++ include/constants.h
@@ -0,0 +1,6 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H 1
+
+const static int PORT = 3333;
+
+#endif
---
+++ src/server.c
@@ -0,0 +1,10 @@
+#include <sys/socket.h>
+#include <stdio.h>
+#include "../include/constants.h"
+
+int main(int argc, char * argv[], char * envp[]) {
+    puts("wtf");
+    puts("lalaala");
+    printf("%d\n", PORT);
+    return 0;
+}
